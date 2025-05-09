fa95acf8a9e1b6652fe7793a4d9255320471656e
VarunSN2002 <varun.sn13@gmail.com>
2025-05-09 11:25:48 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,8 +2,8 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "John Doe",
-  initials: "JD",
+  name: "Varun S N",
+  initials: "VSN",
   url: "https://example.com",
   location: "San Francisco, CA",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
